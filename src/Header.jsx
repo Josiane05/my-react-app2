efa8fcc710cfd9d5d3f31cf88fb0f2d00890b703
Josiane05 <josianemushimiyimana05@gmail.com>
2025-08-30 08:30:05 +0100

    creating navigation,unordered list and list items

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,6 +4,14 @@ function Header(){
     return(
         <header>
             <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Service</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
         </header>
     );
 }
