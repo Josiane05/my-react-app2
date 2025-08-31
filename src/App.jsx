8f3d39e15d99867c6f377779b285e226e802781a
Josiane05 <josianemushimiyimana05@gmail.com>
2025-08-31 17:36:17 +0100

    new file card .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
+
+
 function App() {
 
-  return(
-        <>
-          <Header/>
-          <Footer/>
-        </>
-  );
 }
 
 export default App
---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card(){
+    return(
+        <div className="card">
+            <img></img>
+            <h2>Josiane</h2>
+            <p>i am web developer</p>
+        </div>
+    );
+}
+
+export default Card
