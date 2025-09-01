dbdba7e38c71bc690450e87cc317c7de2f68c5d9
Josiane05 <josianemushimiyimana05@gmail.com>
2025-09-01 09:47:41 +0100

    new file creation card.jsx

---
+++ src/assets/Card.jsx
@@ -0,0 +1,12 @@
+
+function card(){
+    return(
+        <div className="card">
+            <img></img>
+            <h2>Josiane</h2>
+            <p></p>
+        </div>
+    );
+}
+
+export default Card
