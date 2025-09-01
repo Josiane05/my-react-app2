5cb3f28e8f7a6930276c473ce4bb2d2d3f0c2d6f
Josiane05 <josianemushimiyimana05@gmail.com>
2025-09-01 11:26:39 +0100

    stylesheet for card

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,7 @@
 function Card(){
     return(
         <div className="card">
-            <img src="https://via.placeholder.com/150"></img>
+            <img className="card-image" src="https://via.placeholder.com/250"></img>
             <h2>Josiane</h2>
             <p>Aspiring Web developer passionate about creating modern, user-friendly websites. Specializing in React and front-end development</p>
         </div>
