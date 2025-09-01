f3e5e4a978a1dcb2c42713a41126f47ffb8c1ad2
Josiane05 <josianemushimiyimana05@gmail.com>
2025-09-01 12:06:44 +0100

    stylesheet for card-text

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,8 +4,8 @@ function Card(){
     return(
         <div className="card">
             <img className="card-image" src={profilePic} alt="profile picture"></img>
-            <h2 className="card.title">Josiane</h2>
-            <p>Aspiring Web developer passionate about creating modern, user-friendly websites. Specializing in React and front-end development</p>
+            <h2 className="card-title">Josiane</h2>
+            <p className="card-text">Aspiring Web developer passionate about creating modern, user-friendly websites. Specializing in React and front-end development</p>
         </div>
     );
 }
