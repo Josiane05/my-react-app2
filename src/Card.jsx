671002d252738cf937f3f724605ea74f5c173a1b
Josiane05 <josianemushimiyimana05@gmail.com>
2025-09-01 11:00:38 +0100

    image

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+
+
+function Card(){
+    return(
+        <div className="card">
+            <img src="https://via.placeholder.com/150"></img>
+            <h2>Josiane</h2>
+            <p>Aspiring Web developer passionate about creating modern, user-friendly websites. Specializing in React and front-end development</p>
+        </div>
+    );
+}
+
+export default Card
