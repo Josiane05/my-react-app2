1b0efbb70dd1caadeff75053dad9bf3232297e8e
Josiane05 <josianemushimiyimana05@gmail.com>
2025-09-02 13:23:37 +0100

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
 import Button from './Button.jsx'
+import Client from './Client.jsx'
+import UserGreeting from './UserGreeting.jsx'
 
 
 function App() {
-
+  
     return(
         <>
           <Header/>
+          <UserGreeting isLoggedIn={true} username="Josiane"/>
           <Card/>
           <Food/>
           <Client name="spongebob" age={30} isClient={true}/>
