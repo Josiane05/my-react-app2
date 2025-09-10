5ed999c7f604eb426c0f676502dab7bf97a99bfb
Josiane05 <josianemushimiyimana05@gmail.com>
2025-09-10 10:15:18 +0100

    convert an array of string into array of object and sort method

--- src/List.jsx
+++ src/List.jsx
@@ -1,8 +1,17 @@
 
 function List(){
 
-    const fruits = ["Apple", "Orange", "Banana", "coconat", "pinapple"];
-    const listItems = fruits.map(fruit => <li>{fruit}</li>);
+    const fruits = [{id: 1, name:"Apple", calories: 95},
+                    {id: 2, name:"Orange", calories: 45},
+                    {id: 3, name:"Banana", calories: 105}, 
+                    {id: 4, name:"coconat", calories: 159}, 
+                    {id: 5, name:"pinapple", calories: 37}];
+
+    fruits.sort((a, b) => a.name.localeCompare(b.name));
+
+    const listItems = fruits.map(fruit => <li key={fruit.id}>
+                                             {fruit.name}: &nbsp;
+                                             <b>{fruit.calories}</b></li>);
 
     return(<ol>{listItems}</ol>);
 }
