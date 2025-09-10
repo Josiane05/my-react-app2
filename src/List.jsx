f8c380776fffe9e6225782e3a24cbead55527ab5
Josiane05 <josianemushimiyimana05@gmail.com>
2025-09-10 09:42:13 +0100

    changing from unordered list into ordered list

--- src/List.jsx
+++ src/List.jsx
@@ -4,6 +4,6 @@ function List(){
     const fruits = ["Apple", "Orange", "Banana", "coconat", "pinapple"];
     const listItems = fruits.map(fruit => <li>{fruit}</li>);
 
-    return(<ul>{listItems}</ul>);
+    return(<ol>{listItems}</ol>);
 }
 export default List
