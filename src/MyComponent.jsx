eba48c73093c9abbf492b3faccde29b10ab345f6
Josiane05 <josianemushimiyimana05@gmail.com>
2025-09-11 20:29:51 +0200

    usestate

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,5 +1,17 @@
+import React, {useState} from 'react';
 
 function MyComponent(){
 
+    let [name, setName] = useState();
+
+    const updateName = () => {
+        name = "spongebob";
+        console.log(name);
+    }
+
+    return(<div>
+               <p>Name: {name}</p>
+               <button onClick={updateName}>set Name</button>
+           </div>)
 }
 export default MyComponent
