a13d0a657468a15e99291a326ca50664ed9a4359
Josiane05 <josianemushimiyimana05@gmail.com>
2025-09-11 20:58:09 +0200

    boolean toggle status

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -4,6 +4,7 @@ function MyComponent(){
 
     const [name, setName] = useState("Guest");
     const [age, setAge] = useState(0);
+    const [isEmployeed, setIsEmployed] = useState(false);
 
     const updateName = () => {
         setName("spongebob");
@@ -14,12 +15,20 @@ function MyComponent(){
         setAge(age + 2);
     }
 
+    const toggleEmployeedStatus = () => {
+        setIsEmployeed(!isEmployeed);
+    }
+
     return(<div>
                <p>Name: {name}</p>
                <button onClick={updateName}>set Name</button>
 
                 <p>Age: {age}</p>
                <button onClick={incrementAge}>incrementAge</button>
+
+               <p>Is employeed: {isEmployeed ? "Yes" : "No"}</p>
+               <button onClick={toggleEmployeedStatus}>Taggle status</button>
+
            </div>)
 }
 export default MyComponent
