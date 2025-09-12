75519b58bc61964939e789654c029c1e473a0558
Josiane05 <josianemushimiyimana05@gmail.com>
2025-09-12 20:49:16 +0200

    OnChange event handler on number

--- src/MyCompon.jsx
+++ src/MyCompon.jsx
@@ -3,14 +3,22 @@ import React, {useState} from 'react';
 function MyCompon(){
 
     const[name, setName] = useState("Guest");
+    const[quantity, setQuantity] = useState("");
 
     function handleNameChange(event){
         setName(event.target.value);
+    }
+
+    function handleQuantityChange(event){
+        setQuantity(encodeURIComponent.target.value);
     }
     
     return(<div>
               <input value={name} onChange={handleNameChange}/>
               <p>Name: {name}</p>
+
+              <input value={quantity} onChange={handleQuantityChange} type="number"/>
+              <p>Quantity: {quantity}</p>
            </div>);
 }
 export default MyCompon
