87be05a919dd35e2ddd8bf10901a7c8e06bf98be
Josiane05 <josianemushimiyimana05@gmail.com>
2025-09-13 10:10:38 +0200

    OnChange event handler on making <radio>

--- src/MyCompon.jsx
+++ src/MyCompon.jsx
@@ -5,7 +5,8 @@ function MyCompon(){
     const[name, setName] = useState("Guest");
     const[quantity, setQuantity] = useState("1");
     const[comment, setComment] = useState("");
-    const[payment,setPayment] = useState("");
+    const[payment,setPayment] = useState("")
+    const[shipping, setShipping] = useState("");
 
     function handleNameChange(event){
         setName(event.target.value);
@@ -21,6 +22,10 @@ function MyCompon(){
 
     function handlePaymentChange(event){
         setPayment(event.target.value);
+    }
+
+    function handleShippingChange(event){
+        setShipping(event.target.value);
     }
     
     return(<div>
@@ -41,6 +46,20 @@ function MyCompon(){
                 <option value="Giftcard">Giftcard</option>
               </select>
               <p>Payment: {payment}</p>
+
+              <label>
+                <input type="radio" value="Pick Up"
+                       checked={shipping === "Pick Up"}
+                       onChange={handleShippingChange}/>
+                Pick Up
+              </label><br/>
+               <label>
+                <input type="radio" value="Delivery"
+                       checked={shipping === "Delivery"}
+                       onChange={handleShippingChange}/>
+                Delivery
+              </label>
+              <p>Shipping: {shipping}</p>
            </div>);
 }
 export default MyCompon
