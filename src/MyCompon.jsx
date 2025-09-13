5c2f77b789e3686cd3367c6fb468776d93a240de
Josiane05 <josianemushimiyimana05@gmail.com>
2025-09-13 09:54:10 +0200

    OnChange event handler on payment options

--- src/MyCompon.jsx
+++ src/MyCompon.jsx
@@ -5,6 +5,7 @@ function MyCompon(){
     const[name, setName] = useState("Guest");
     const[quantity, setQuantity] = useState("1");
     const[comment, setComment] = useState("");
+    const[payment,setPayment] = useState("");
 
     function handleNameChange(event){
         setName(event.target.value);
@@ -17,6 +18,10 @@ function MyCompon(){
     function handleCommentChange(event){
         setComment(event.target.value);
     }
+
+    function handlePaymentChange(event){
+        setPayment(event.target.value);
+    }
     
     return(<div>
               <input value={name} onChange={handleNameChange}/>
@@ -28,6 +33,14 @@ function MyCompon(){
               <textarea value={comment} onChange={handleCommentChange}
               placeholder="Enter delivery instructions"/>
               <p>Comment: {comment}</p>
+
+              <select value={payment} onChange={handlePaymentChange}>
+                <option value="">Select an option</option>
+                <option value="Visa">Visa</option>
+                <option value="Mastercard">Mastercard</option>
+                <option value="Giftcard">Giftcard</option>
+              </select>
+              <p>Payment: {payment}</p>
            </div>);
 }
 export default MyCompon
