62614457262fa6dc3aceb5801519821b372f52fd
Josiane05 <josianemushimiyimana05@gmail.com>
2025-09-15 13:07:54 +0200

    functions of todolist

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,6 +2,29 @@ import React, { useState } from 'react'
 
 function ToDoList(){
 
+    const[tasks, setTasks] = useState([]);
+    const[newTask, setNewTask] = useState("");
+
+    function handleInputChange(event){
+
+    }
+
+    function addTask(){
+
+    }
+
+    function deleteTask(index){
+
+    }
+
+    function moveTaskUp(index){
+
+    }
+
+    function moveTaskDown(index){
+        
+    }
+
     return(<></>);
 
 }
