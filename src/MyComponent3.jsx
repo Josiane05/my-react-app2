6c22a1a9650d76885af31ad8fa81fe66949bdfe9
Josiane05 <josianemushimiyimana05@gmail.com>
2025-09-15 11:39:11 +0200

    .mamp element to get ordered list

--- src/MyComponent3.jsx
+++ src/MyComponent3.jsx
@@ -1,6 +1,22 @@
-
+import React, { useState} from 'react'
 
 function MyComponent3(){
 
+    const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
+
+    function handleAddFood(){
+
+    }
+
+    function handleRemoveFood(){
+
+    }
+
+    return (<div>
+                <h1>List Of Food</h1>
+                <ul>
+                    {foods.map((food, index) => <li></li>)}
+                </ul>
+            </div>);
 }
-export default MyComponent3
+export default MyComponent3;
